11ccfea4d09c779f0fb2bb38cec530d01fef4c12
Daniel Mendez <dev@lodalo.com>
2014-03-26 22:30:05 -0400

    Adds ember embedded application and component with action

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+App = Ember.Application.create({
+    rootElement: '#wnp-ember-widget'
+});
+
+App.Router = Ember.Router.extend({
+  location: 'none'
+});
+
+books = [{
+  title: "The Ember way",
+  description: "There are many things to learn, but to learn the right thing is best."
+}, {
+  title: "When you clicked",
+  description: "Once upon a time, a user clicked on a page div..."
+}];
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return books; 
+  }
+});
+
+App.LibraryBookComponent = Ember.Component.extend({
+    actions: {
+        toggleDescription: function(){
+			this.toggleProperty('showDescription');
+        }
+    }
+});
